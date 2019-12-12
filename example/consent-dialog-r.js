6187c061d9cb29114d6df0875ae14c3c5eca79f6
Hajime Sano <hajime.sano@treasure-data.com>
2019-12-12 17:43:31 +0900

    fixed

--- example/consent-dialog-r.js
+++ example/consent-dialog-r.js
@@ -1,12 +1,10 @@
-function trackerAcceptanceProcess() {
- // put your TD tag here.
+// Put your TD tag here
 
 
 
-}
-
+ // Wrapper for TD's optoup method
 functino optoutTd(){
-  td.blockEvents(); // TD's optoup method
+  td.blockEvents();
 }
 
 (function() {
@@ -14,13 +12,12 @@ functino optoutTd(){
     return ((';' + document.cookie + ';').match(';' + key + '=([^¥S;]*)') || [])[1];
   };
 
-  if (readCookie('_td_optout') !== '1') {
-    trackerAcceptanceProcess();
-  } else {
+  var optoutFlag = readCookie('_td_optout');
+  if (optoutFlag !== '0' && optoutFlag !== '1' ) {
     var dialogDiv = document.createElement('div');
     dialogDiv.style = 'position: fixed; bottom: 0; right: 0; width: 260px; margin: 0px; padding: 20px; border: 1px solid #ccc; background: #555; color: #ccc; font-size: 10pt';
     dialogDiv.innerHTML = '<p>Please support us to measure our service quality. Your usage is measured but you can opt-out by clicking the below button. </p>';
-    dialogDiv.innerHTML += '<button onClick="document.cookie=\'_td_optout=1\';optoutTd();this.parentElement.remove();">Optout</button>';
+    dialogDiv.innerHTML += '<button onClick="document.cookie=\'_td_optout=1\';optoutTd();this.parentElement.remove();">Optout</button> <button onClick="document.cookie=\'_td_optout=0\';this.parentElement.remove();">Continue</button>';
     var scriptTag = document.currentScript || (function() {
       var tags = document.getElementsByTagName('script')
       return tags.item(tags.length - 1)
